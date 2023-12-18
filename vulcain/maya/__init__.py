0e0940fc18b29230c16717a9efca6c1d99375620
amaury.hacquard <amaury.hacquard@gmail.com>
2023-12-18 22:02:50 +0100

    add : maya `__init__.py` file

---
+++ vulcain/maya/__init__.py
@@ -0,0 +1,8 @@
+from .shared import init_new_scene, create_groups_from_dict
+from .plugin_loader import load_plugin
+
+__all__ = [
+    "init_new_scene",
+    "create_groups_from_dict",
+    "load_plugin"
+]
